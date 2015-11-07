fd356cee566f853519e9fdc2d875015d4d911cfe
David Sulpy <david@sulpy.com>
2015-11-07 15:38:33 -0600

    Fixed incorrectly named json keys

--- src/main/java/com/alonkadury/initialState/Bucket.java
+++ src/main/java/com/alonkadury/initialState/Bucket.java
@@ -2,19 +2,19 @@ package com.alonkadury.initialState;
 
 public class Bucket implements Events {
   private final static String BUCKET_API_URL = API_BASEURL + "buckets";
-  private String key;
+  private String bucketKey;
   private String bucketName;
 
-  public Bucket(String key) {
-    this(key, null);
+  public Bucket(String bucketKey) {
+    this(bucketKey, null);
   }
 
-  public Bucket(String key, String friendlyName) {
-    this.key = key;
+  public Bucket(String bucketKey, String friendlyName) {
+    this.bucketKey = bucketKey;
     this.bucketName = friendlyName;
   }
 
-  public String getKey() { return key; }
+  public String getKey() { return bucketKey; }
   //public String getFriendlyName() { return friendlyName; }
 
   public String getEndpoint() {
